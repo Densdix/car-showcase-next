6eb959c0acf287ea9c9cb80a2691ede44ff68bb6
Densdix <dixfun333333@gmail.com>
2023-06-26 15:57:24 +0300

    updated styles

--- components/CarCard/CarDetails.tsx
+++ components/CarCard/CarDetails.tsx
@@ -10,7 +10,7 @@ const CarDetails: React.FC<ICarDetails> = ({ car, isOpen, closeModal }) => {
     return (
         <>
             <Transition appear show={isOpen} as={Fragment}>
-                <Dialog as='div' className='relative z-30' onClose={closeModal}>
+                <Dialog as='div' className='relative z-40' onClose={closeModal}>
                     <Transition.Child as={Fragment}
                         enter='ease-out duration-300'
                         enterFrom='opacity-0'
